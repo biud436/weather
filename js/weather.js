101dbb90674067ef5cb7429a42f2a68d4023f153
biud436 <biud436@gmail.com>
2020-09-25 15:22:26 +0900

    .

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,19 @@
-
+const API_KEY = "f5f963901bc7a7df56b73323ca00ae47";
+const config = {
+    getApiSrc(loc) {
+        return `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${API_KEY}`
+    },
+    DATA: "./data/kr.json"
+}
 
 class App {
     constructor() {
         this.init();
-        this.update();   
     }
 
-    getApiSrc(loc) {
-        return `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=f5f963901bc7a7df56b73323ca00ae47`
-    };
-    
     loadKR() {
 
-        $.getJSON("./data/kr.json", (data) => {
+        $.getJSON(config.DATA, (data) => {
     
             const sel = $("<select></select>", {
                 name: "wow"
@@ -28,13 +29,13 @@ class App {
                 sel.append(opt);
             });
     
-            sel.on("change", (ev) => {
+            sel.on("change", ev => {
                 const coord = $("option:selected").attr("coord");
     
                 const myCoord = JSON.parse(coord);
                 const lat = myCoord.lat;
                 const lng = myCoord.lon;
-                const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=f5f963901bc7a7df56b73323ca00ae47`;
+                const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
     
                 this.loadJson(url);
     
@@ -65,14 +66,26 @@ class App {
                     const temp_min = main.temp_min;
                     const temp_max = main.temp_max;
     
+                    // 지역 이름
                     $(".city").text(`${city}/${country}`);
+
+                    // 날씨
                     $("p.icon").text(wmain);
+
+                    // 날씨 아이콘 변경
                     $("figure.icon").empty();
                     $("figure.icon").append(`<img src="https://openweathermap.org/img/wn/${icon}@4x.png">`);
+
+                    // 기온
                     $("p.temp").text((temp - 273.15 >> 0) + "°");
+
+                    // 최고 기온
                     $("p.temp_max").html(Math.floor(temp_max - 273.15) + "&deg max");
+
+                    // 최저 기온
                     $("p.temp_min").text(Math.floor(temp_min - 273.15) + "° max");
     
+                    // 셀렉트 박스 설정
                     const children = Array.from($("select").children());
                     const currentIndex = $(children.filter(i => i.innerHTML.indexOf(city) >= 0)[0]).index();
                     $("select")[0].selectedIndex = currentIndex;
@@ -94,18 +107,22 @@ class App {
             this.loadJson(url);
             this.loadKR();
     
-        }, (error) => {
-            this.loadJson(this.getApiSrc("Seoul"));
+        }, error => {
+            this.loadJson(config.getApiSrc("Seoul"));
             this.loadKR();
         });
     
     }
 }
 
+
+
 function update(dt) {
     $("#weather_info").css("background-position", (dt * 0.007) + "px");
 
     window.requestAnimationFrame(update);
 }
+
+const app = new App();
 
 update();
