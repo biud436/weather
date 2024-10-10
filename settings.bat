1321e396f91fc8f6efa284fed3877320212a4bd8
biud436 <biud436@gmail.com>
2024-10-11 00:36:10 +0900

    chore: remove webpack bat file

--- settings.bat
+++
@@ -1,5 +0,0 @@
-npm intall -y
-npm install -D webpack webpack-cli
-npm install --save-dev style-loader css-loader
-npm install --save-dev file-loader
-powershell wget "https://github.com/biud436/InitialEditor/raw/master/webpack.config.js" -OutFile "webpack.config.js"
